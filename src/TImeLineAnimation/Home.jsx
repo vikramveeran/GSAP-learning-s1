94e5ffdaf117c76309eddd7d55f768e3f8735ae8
vikramveeran <vikram.shanthipura@gmail.com>
2026-01-31 15:36:33 +0530

    fith commit

--- src/TImeLineAnimation/Home.jsx
+++ src/TImeLineAnimation/Home.jsx
@@ -5,27 +5,43 @@ import gsap from "gsap";
 import {ScrollTrigger} from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 
-const t1 = gsap.timeline();
+
 
 // t1.to(".homeImg1",{x:-300,duration:2})
 
 
-const Home = () => {
-    useGSAP(()=>{
-t1.to(".homeImg1",{x:-300,duration:2},)
-   .to( ".homeImg1",{x:300,duration:3},)
 
-},[])
+const Home = () => {
+    
+  useGSAP(()=>{
+     gsap.to('.name span',{
+
+      scrollTrigger:{
+        trigger:'.page2',
+        backgroundColor:"red",
+      },
+        y:20,
+         opacity:1,
+         stagger:-0.1,
+        //  duration:2,
+       repeat:-1,
+       yoyo:true,
+     })
+  })
+
   return (
-    <div className=' w-full h-full overflow-x-hidden'>
-      <div className='homeImg  absolute bg-[url(https://images.unsplash.com/photo-1612404748636-568ca4155994?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2t5JTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D)] w-full h-182 bg-cover overflow-x-hidden'>
-      <div className= 'homeImg1 blur-sm bg-[url(https://images.unsplash.com/photo-1612404748636-568ca4155994?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2t5JTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D)]  w-70  h-180 -top-0.5 -right-379 relative '>
-         
+    <div  >
+      <div className='page1 w-full h-[100vh] bg-fuchsia-700'>
+        
       </div>
+      <div className='page2 w-full h-[100vh] bg-black '>
+          <h1 className='name relative text-white flex justify-center top-100 items-center space-x-10'><span>V</span><span>I</span><span>K</span><span>R</span><span>A</span><span>M</span></h1>
       </div>
     </div>
   )
 }
 
 export default Home
+
+
 
